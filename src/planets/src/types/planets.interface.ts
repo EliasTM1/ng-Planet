bb8bb720ff075d72a7be436eea7b162b13ae342a
HM1 <mrtoscano94@gmail.com>
2023-01-13 10:12:28 -0500

    Mocks created and assets included

---
+++ src/planets/src/types/planets.interface.ts
@@ -0,0 +1,16 @@
+interface Planet{
+  name: string;
+  overview: string;
+  overViewUrl: string;
+  internal: string;
+  internalUrl: string;
+  surface: string;
+  surfaceUrl: string;
+  rotationTime: number;
+  revolutionTime : number;
+  radius : number;
+  averageTemp: number,
+  tempUnit: string
+}
+
+
